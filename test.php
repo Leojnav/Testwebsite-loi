3cc1dd3f819b33b69d7da0c2f2d125456f00348a
Leojnav <94356230+Leojnav@users.noreply.github.com>
2023-11-02 17:00:37 +0100

    extra

--- test.php
+++ test.php
@@ -96,6 +96,25 @@ echo $out;
     global $name; $name = ucfirst(strtolower($name));
     global $lastname;$lastname = ucfirst(strtolower($lastname));
   }
+  class User
+  {
+    public $name, $password;
+
+    function save_user()
+    {
+  	  echo "Save User code goes here";
+    }
+  }
+  $object = new User;
+  print_r($object); echo "<br>";
+
+  $object->name = "Joe";
+  $object->password = "mypass";
+  print_r($object); echo "<br>";
+
+  $object->save_user();
+
+
 ?>
 </section>
 
